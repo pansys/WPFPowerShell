aac949a0c89190f530e79abfc7272b310eae7093
Mathew Panikassery <mathew.p@outlook.com>
2022-09-20 22:03:52 +0530

    Comments

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -12,11 +12,11 @@ Class MainWindow
 
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         Dim startInfo As New ProcessStartInfo("powershell.exe")
-        startInfo.WindowStyle = ProcessWindowStyle.Hidden
+        startInfo.WindowStyle = ProcessWindowStyle.Hidden 'this is not working, need fine tuning
 
         Dim p As System.Diagnostics.Process = Process.Start(startInfo)
-        'p.WaitForInputIdle()
-        Threading.Thread.Sleep(1000)
+        'p.WaitForInputIdle() 'If this works below sleep is not required and embedding external window is quicker
+        Threading.Thread.Sleep(1000) 'Change this to 100 to avoid poping up the external powershell
         SetParent(p.MainWindowHandle, New WindowInteropHelper(Application.Current.MainWindow).Handle)
         SendMessage(p.MainWindowHandle, WM_SYSCOMMAND, SC_MAXIMIZE, 0)
         p.StartInfo.WindowStyle = ProcessWindowStyle.Normal
