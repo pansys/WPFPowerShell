b3ae7a50655c475715f8ac01d0bf1cac39afa88f
Mathew Panikassery <mathew.p@outlook.com>
2022-09-27 11:14:55 +0530

    Clean code

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -14,11 +14,7 @@ Class MainWindow
     Declare Auto Function GetWindowLong Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nIndex As Integer) As Integer
     Declare Auto Function SetWindowLong Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nIndex As Integer, ByVal dwNewLong As Integer) As Integer
     Declare Auto Function SetWindowPos Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal hWndInsertAfter As IntPtr, ByVal X As Integer, ByVal Y As Integer, ByVal cx As Integer, ByVal cy As Integer, ByVal uFlags As Integer) As Boolean
-
-
-    Private Const WM_CLOSE As Integer = 16
-    Private Const WM_SYSCOMMAND As Integer = 274
-    Private Const SC_MAXIMIZE As Integer = 61488
+    Declare Auto Function ShowWindow Lib "user32.dll" (ByVal hWnd, ByVal nCmdShow) As Boolean
 
     Const WS_BORDER As Integer = 8388608
     Const WS_DLGFRAME As Integer = 4194304
@@ -30,49 +26,23 @@ Class MainWindow
     Const GWL_STYLE As Integer = -16&
     Const GWL_EXSTYLE As Integer = -20&
     Const WS_EX_DLGMODALFRAME As Integer = &H1L
-    Const SWP_NOMOVE As Integer = &H2
-    Const SWP_NOSIZE As Integer = &H1
-    Const SWP_FRAMECHANGED As Integer = &H20
-    Const MF_BYPOSITION As UInteger = &H400
-    Const MF_REMOVE As UInteger = &H1000
-
-    'Private Const GWL_STYLE As Integer = -16
-    'Private Const WS_CHILD As uint = 0x40000000 
-    'Private Const WS_BORDER = 0x00800000 'window With border
-    'Private Const WS_DLGFRAME = 0x00400000 'window With Double border but no title
 
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         Dim startInfo As New ProcessStartInfo("powershell.exe")
-        startInfo.WindowStyle = ProcessWindowStyle.Hidden 'this is not working, need fine tuning
+        startInfo.WindowStyle = ProcessWindowStyle.Hidden
 
         Dim p As System.Diagnostics.Process = Process.Start(startInfo)
-        'p.WaitForInputIdle() 'If this works below sleep is not required and embedding external window is quicker
-        Threading.Thread.Sleep(1000) 'Change this to 100 to avoid poping up the external powershell
-        'SetParent(p.MainWindowHandle, New WindowInteropHelper(Application.Current.MainWindow).Handle)
 
         Dim psPanel As Forms.Panel = New System.Windows.Forms.Panel()
         psPanel.Width = 1180
         psPanel.Height = 500
         winFormsHost.Child = psPanel
 
-        Dim prnt As IntPtr = GetParent(p.MainWindowHandle)
-        'Dim pt As System.Drawing.Point = New System.Drawing.Point(100, 100)
-        'Do While psPanel.Controls.Count = 0
-        SetParent(p.MainWindowHandle, psPanel.Handle)
-        Dim prnt2 As IntPtr = GetParent(p.MainWindowHandle)
-        'p.StartInfo.WindowStyle = ProcessWindowStyle.Maximized
-        'SendMessage(p.MainWindowHandle, WM_SYSCOMMAND, SC_MAXIMIZE, 0)
+        Threading.Thread.Sleep(1000) 'change to 100 to avoid poping up the external window
+        Dim success As Integer = SetParent(p.MainWindowHandle, psPanel.Handle)
         MakeExternalWindowBorderless(p.MainWindowHandle)
         MoveWindow(p.MainWindowHandle, 0, 0, psPanel.Width, psPanel.Height, True)
-        'SetWindowLong(p.MainWindowHandle, GWL_EXSTYLE, Style Or WS_EX_DLGMODALFRAME)
-
-
-        'Threading.Thread.Sleep(100)
-        'Exit Do
-        'Loop
-
     End Sub
-
 
     Public Sub MakeExternalWindowBorderless(ByVal MainWindowHandle As IntPtr)
         Dim Style As Integer
@@ -85,7 +55,6 @@ Class MainWindow
         SetWindowLong(MainWindowHandle, GWL_STYLE, Style)
         Style = GetWindowLong(MainWindowHandle, GWL_EXSTYLE)
         SetWindowLong(MainWindowHandle, GWL_EXSTYLE, Style Or WS_EX_DLGMODALFRAME)
-        'SetWindowPos(MainWindowHandle, New IntPtr(0), 0, 0, 0, 0, SWP_NOMOVE Or SWP_NOSIZE Or SWP_FRAMECHANGED)
     End Sub
 End Class
 
