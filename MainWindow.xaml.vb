546e98f0a4872f3c7c1527d1187423ca22c285c4
Mathew Panikassery <mathew.p@outlook.com>
2022-09-29 14:32:28 +0530

    Fill tab and resize powershell along with window resize

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -2,6 +2,7 @@
 Imports System.Management.Automation
 Imports System.Management.Automation.Runspaces
 Imports System.Text
+Imports System.Windows.Forms
 Imports System.Windows.Interop
 
 Class MainWindow
@@ -10,6 +11,8 @@ Class MainWindow
     Declare Auto Function SendMessage Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
     Declare Auto Function MoveWindow Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer, ByVal nHeight As Integer, ByVal bRepaint As Boolean) As Boolean
     Declare Auto Function PostMessage Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Boolean
+    Declare Auto Function SetForegroundWindow Lib "user32.dll" (ByVal hwnd As Integer) As Integer
+    Declare Auto Function GetDesktopWindow Lib "user32.dll" () As Long
 
     Declare Auto Function GetWindowLong Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nIndex As Integer) As Integer
     Declare Auto Function SetWindowLong Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nIndex As Integer, ByVal dwNewLong As Integer) As Integer
@@ -27,21 +30,32 @@ Class MainWindow
     Const GWL_EXSTYLE As Integer = -20&
     Const WS_EX_DLGMODALFRAME As Integer = &H1L
 
+    Const SW_HIDE As Integer = 0
+    Const SW_RESTORE As Integer = 9
+    Const SW_MINIMIZE As Integer = 6
+    Const SW_SHOWMINIMIZED As Integer = 2
+
+    Private Const WM_SYSCOMMAND As Integer = 274
+    Private Const SC_MAXIMIZE As Integer = 61488
+
+    Dim psHandle As IntPtr
+    Dim psPanel As Forms.Panel
     Private Sub Grid_Loaded(sender As Object, e As RoutedEventArgs)
         Dim startInfo As New ProcessStartInfo("powershell.exe")
         startInfo.WindowStyle = ProcessWindowStyle.Hidden
+        'startInfo.CreateNoWindow = True
+        'startInfo.UseShellExecute = True
 
         Dim p As System.Diagnostics.Process = Process.Start(startInfo)
 
-        Dim psPanel As Forms.Panel = New System.Windows.Forms.Panel()
-        psPanel.Width = 1180
-        psPanel.Height = 500
+        psPanel = New System.Windows.Forms.Panel()
+        psPanel.Dock = DockStyle.Fill
         winFormsHost.Child = psPanel
 
-        Threading.Thread.Sleep(1000) 'change to 100 to avoid poping up the external window
+        Threading.Thread.Sleep(1000)
         Dim success As Integer = SetParent(p.MainWindowHandle, psPanel.Handle)
         MakeExternalWindowBorderless(p.MainWindowHandle)
-        MoveWindow(p.MainWindowHandle, 0, 0, psPanel.Width, psPanel.Height, True)
+        psHandle = p.MainWindowHandle
     End Sub
 
     Public Sub MakeExternalWindowBorderless(ByVal MainWindowHandle As IntPtr)
@@ -55,6 +69,10 @@ Class MainWindow
         SetWindowLong(MainWindowHandle, GWL_STYLE, Style)
         Style = GetWindowLong(MainWindowHandle, GWL_EXSTYLE)
         SetWindowLong(MainWindowHandle, GWL_EXSTYLE, Style Or WS_EX_DLGMODALFRAME)
+    End Sub
+
+    Private Sub winFormsHost_SizeChanged(sender As Object, e As SizeChangedEventArgs) Handles winFormsHost.SizeChanged
+        MoveWindow(psHandle, 0, 0, psPanel.Width, psPanel.Height, True)
     End Sub
 End Class
 
